1f43e60f13a74d0ac8df92e496b28a9ac8084039
chronosgit <warfacemeister@mail.ru>
2024-01-31 21:59:23 +0600

    Create four wine items

--- src/db/wines.tsx
+++ src/db/wines.tsx
@@ -1,7 +1,34 @@
 import IWineItem from "../interfaces/IWineItem";
 
 const wines: IWineItem[] = [
-
+    {
+        title: 
+            `Fortnum's South Island
+            Sauvignon Blanc, James Thompson, 75cl`,
+        price: "29$",
+        imageUrl: "../../public/images/wines/bottle-1.png",
+    },
+    {
+        title: 
+            `Fortnum's North Island
+            Sauvignon Blanc, James Thompson, 75cl`,
+        price: "29$",
+        imageUrl: "../../public/images/wines/bottle-2.png",
+    },
+    {
+        title: 
+            `Fortnum's West Island
+            Sauvignon Blanc, James Thompson, 75cl`,
+        price: "29$",
+        imageUrl: "../../public/images/wines/bottle-3.png",
+    },
+    {
+        title: 
+            `Fortnum's East Island
+            Sauvignon Blanc, James Thompson, 75cl`,
+        price: "29$",
+        imageUrl: "../../public/images/wines/bottle-4.png",
+    },
 ];
 
 export default wines;
